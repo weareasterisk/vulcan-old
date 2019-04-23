da16f2366d2b08806348eb65e4f23cf40fc5436c
Muhammad Khatri <khizar3840@gmail.com>
2019-04-22 21:24:02 -0500

    Made read function non-blocking

--- services/utils/csv.js
+++ services/utils/csv.js
@@ -3,10 +3,14 @@ const csv2json = require('./dependencies/csv2json');
 
 const json2csv = require('./dependencies/json2csv');
 
-module.exports.read = (file) => {
-  const data = fs.readFileSync(file.path, { encoding: 'utf8' });
+module.exports.read = async (file) => {
+  const data = await fs.readFile(file.path, { encoding: 'utf8' }, function(err, contents) {
+    if (err)
+      throw err;
+    return contents;
+  });
   fs.unlinkSync(file.path);
-  return csv2json(data, {parseNumbers: false})
+  await csv2json(data, {parseNumbers: false})
 };
 
 module.exports.tocsv = (json) => {
