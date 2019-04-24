99e2c17068376338367b5a1f271740c218c20611
Muhammad Khatri <khizar3840@gmail.com>
2019-04-24 14:26:02 -0500

    disable eslint warnings and remove console statements

--- api/routes/devpost.js
+++ api/routes/devpost.js
@@ -19,28 +19,17 @@ router.post('/csv', upload.single('file'), async (req, res, next) => {
     const data = await read(file);
 
     const parameters = JSON.parse(req.body.parameters);
-    console.log(data);
 
     const gavelData = await construct(data, parameters);
 
     res.send(tocsv(gavelData));
-    console.log(JSON.stringify(gavelData));
   }
   catch (error) {
     fs.unlinkSync(file.path);
+    // eslint-disable-next-line no-console
     console.error(error);
     res.status(400).send(error)
   }
-
-  /*{
-    description: [
-      "Describe Your Hack In 140 Characters. Target This To A 5th Grader.",
-      "Describe Your Hack In 140 Characters. Target This To A Peer.",
-      "Describe Your Hack In 140 Characters. Target This To A Senior Engineer With Years Of Experience."
-    ],
-    location: "What's Your Table Number? Format Is Floor #, Section #, Row/Table #S, I.E. 1 1 A1."
-  };*/
-
 });
 
 
